59e23fe29d34bb6199137eb3a878c2ba9ce93dfa
Jorge2Echemendia <jorge2echemendia@gmail.com>
2026-09-25 11:55:19 -0400

    Corregir enlaces

--- frontend-blazor/KeyForge.Frontend/Program.cs
+++ frontend-blazor/KeyForge.Frontend/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://key-forge-kappa.vercel.app")  });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://key-forge-kappa.vercel.app/")  });
 builder.Services.AddMudServices(config =>
 {
     config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
